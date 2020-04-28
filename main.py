b97f9f6d773217e1dce73acba1064ce93d2dfbb2
Sabard Pierre <pierresabard@gmail.com>
2020-04-28 23:09:40 +0200

    Première version de la modelisation du jeu ( en mieux)

--- main.py
+++ main.py
@@ -3,20 +3,20 @@
 from Partie import Partie
 
 
-def Jouer(nombreCases, stockPierres, strategieDroit, strategieGauche, precendents = [], precedentsMirroirs = [], affichage = True):
-
-    partie = Partie(nombreCases, stockPierres)
-
-    enCours = True
-
-    while enCours:
-        if affichage:
-            print(partie.Resume())
-
-        try :
-            exceptionGauche = False
-            exceptionDroite = False
-
-            
+# def Jouer(nombreCases, stockPierres, strategieDroit, strategieGauche, precendents = [], precedentsMirroirs = [], affichage = True):
+#
+#     partie = Partie(nombreCases, stockPierres)
+#
+#     enCours = True
+#
+#     while enCours:
+#         if affichage:
+#             print(partie.Resume())
+#
+#         try :
+#             exceptionGauche = False
+#             exceptionDroite = False
+#
+#
 
 
