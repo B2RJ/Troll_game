ec8172cef30b9ae385b27be7129c32c54af39062
BRUNEAU RICHARD p1402059 <richard.bruneau@etu.univ-lyon1.fr>
2020-05-19 15:17:07 +0200

    new main.py

--- main.py
+++ main.py
@@ -19,4 +19,33 @@ from Partie import Partie
 #
 #
 
+#Debut CODE 19/05
+
+def main():
+    list = []
+    nbVictoire1 = 0
+    nbVictoire2 = 0
+    raw = 0
+
+    nbPartie = 10;
+    Pierre = Player("Pierre")
+    Richard = Player("Richard")
+
+    for i in range(0, nbPartie) :
+        jeu = Game(Pierre, Richard, 31, 11)
+        list.append(jeu.getWinner())
+        print(jeu.getWinner())
+
+    for j in range(0, nbPartie) :
+        if list[j] == Pierre.pseudo :
+            nbVictoire1 = nbVictoire1 + 1
+        if list[j] == Richard.pseudo : 
+            nbVictoire1 = nbVictoire1 + 1
+        if list[j] == "Egalite" :
+            raw = raw + 1
+
+    print("Pierre a gagne ", nbVictoire1, " fois.")
+    print("Richard a gagne ", nbVictoire2, " fois")
+    print("Il y a eu ", raw, "egalités")        
+
 
