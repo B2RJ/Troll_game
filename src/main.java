8c10952b0b4cf5395efde02e14a25dbe417d9333
BRUNEAU RICHARD p1402059 <richard.bruneau@etu.univ-lyon1.fr>
2020-08-05 11:05:07 +0200

    IA vs Man is done

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * The main class
  * @author B2RJ and Pierre SABARD
@@ -12,17 +14,59 @@ public class main {
      */
 	public static void main(String[] args) throws Exception {
 
-	    int nbGames = 10;
-        
-        Board p1 = new Board(20, 7);
-        Strategy s1 = new StrategySafe(p1);
-        Strategy s2 = new StrategyRandom(p1);
-        
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        while (choice < 1 || choice > 6) {
+            System.out.println("What kind of games do you want to play");
+            System.out.println("1 : IA Safe vs IA Safe");
+            System.out.println("2 : IA Safe vs IA Random");
+            System.out.println("3 : IA Random vs IA Random");
+            System.out.println("4 : IA Safe vs Human");
+            System.out.println("5 : IA Random vs Human");
+            //Read the choice
+            choice = sc.nextInt();
+            if (choice < 1 || choice > 6) {
+                System.out.println("Hmm, something wrong");
+            }
+        }
+        System.out.println("You choose: " + choice);
+
+        int nbGames = 10;
+        Board b1 = new Board(20, 7);
+
+        Strategy s1, s2;
+
+/*        s1 = new StrategySafe(b1);
+        s2 = new StrategySafe(b1);*/
+
+        switch(choice) {
+            case 1:
+                s1 = new StrategySafe(b1);
+                s2 = new StrategySafe(b1);
+                break;
+            case 2:
+                s1 = new StrategySafe(b1);
+                s2 = new StrategyRandom(b1);
+                break;
+            case 3:
+                s1 = new StrategyRandom(b1);
+                s2 = new StrategyRandom(b1);
+                break;
+            case 4:
+                s1 = new StrategySafe(b1);
+                s2 = new StrategyHuman(b1);
+                break;
+            default:
+                s1 = new StrategyRandom(b1);
+                s2 = new StrategyHuman(b1);
+                break;
+        }
+
         int nbJ1 = 0;
         int nbJ2 = 0;
         int nbEq = 0;
         
-        Game game = new Game(p1, s1, s2);
+        Game game = new Game(b1, s1, s2);
         System.out.print("[");
         for (int i = 0; i < nbGames; i++) {
             System.out.print(".");
@@ -31,11 +75,14 @@ public class main {
             switch (game.getWinner()) {
                 case 0:
                     nbEq++;
+                    System.out.println("Tie");
                     break;
                 case 1:
                     nbJ1++;
+                    System.out.println("Player 1 win");
                     break;
                 case 2:
+                    System.out.println("Player 2 win");
                     nbJ2++;
                     break;
             }
